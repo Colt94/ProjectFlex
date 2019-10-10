a5bb653b9e00fd75b949a497984c6edaa5aae72c
Ubuntu <ubuntu@ip-172-31-29-22.us-east-2.compute.internal>
2019-10-10 20:38:23 +0000

    Migrated/seeded users table

--- ProjectFlex/db/seeds.rb
+++ ProjectFlex/db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user_list = [
+    ["Grant", "exec"],
+    ["Emily", "exec"],
+    ["Kelsey", "exec"],
+    ["Monty", "exec"],
+    ["Kelly", "exec"],
+    ["Ashley", "zl"],
+    ["Jacob", "zl"],
+    ["Catherine", "zl"],
+    ["Maggie", "member"],
+    ["Evan", "member"],
+    ["Macy", "member"],
+    ["Alex", "member"],
+    ["Gia", "member"]
+]
+
+user_list.each do |name, permission|
+    User.create(name: name, permissions: permission)
+end
