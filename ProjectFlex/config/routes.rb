a47d82209cc0dba861e3f011000c9c2437a39dcd
Ubuntu <josephsantanapr@gmail.com>
2019-10-10 16:04:19 +0000

    Moved homepage to static

--- ProjectFlex/config/routes.rb
+++ ProjectFlex/config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get "/", to: "homepage#home", as: "root"
+  get "/", to: "static#home", as: "root"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
