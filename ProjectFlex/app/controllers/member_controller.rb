c7077585e040f2b73cc7fd31e6af54ae8d35da0f
Colt94 <coltonmonts@gmail.com>
2019-10-13 17:30:13 +0000

    session testing

--- ProjectFlex/app/controllers/member_controller.rb
+++ ProjectFlex/app/controllers/member_controller.rb
@@ -4,6 +4,7 @@ class MemberController < ApplicationController
     
     def home
         @username = session[:cas_user]
+        session[:cas_user] = @username
         #@username = "Coltmo"
         render :template => 'static/home'
     end
