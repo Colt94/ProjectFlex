2ae1120986d465767b0f0c65969fcec7cb45a675
brettamartin7 <brettamartin7@gmail.com>
2019-10-31 21:40:47 +0000

    merge fix

--- ProjectFlex/app/models/user.rb
+++ ProjectFlex/app/models/user.rb
@@ -5,20 +5,11 @@ class User < ApplicationRecord
     # def self.get_ids
     #     User.select("id")
     # end
-<<<<<<< HEAD
-    def self.get_permissions
-     User.select("permissions") 
-    end
-    
-    def self.get_user(username) 
-        User.find_by(net_id: username)
-=======
     # def self.get_permissions
     #     User.select("permissions") 
     # end
     def self.get_user(netid) 
         User.find_by(net_id: netid)
->>>>>>> be8900592faf4473077da5c97b5238841ea9e47a
     end
     
     
