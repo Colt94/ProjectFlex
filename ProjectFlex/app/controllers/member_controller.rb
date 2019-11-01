101ce261a66bfdf353fc273311988e0290a6f715
Ubuntu <josephsantanapr@gmail.com>
2019-11-01 16:40:35 +0000

    Began registrations page

--- ProjectFlex/app/controllers/member_controller.rb
+++ ProjectFlex/app/controllers/member_controller.rb
@@ -69,23 +69,33 @@ class MemberController < ApplicationController
     def mypoints
         netid = session[:cas_user]
         @user = User.get_user(netid)
-        @user_points = [];
+        @user_points = []
         
         events_attended = EventAttendance.find_registered_events(netid, "approved")
         events_attended.each{ |event_attended|
             @user_points.push(Event.find(event_attended.event_id))
         }
-        @fr_points = @user_points.select{ |event| event.point_type == "fr"}; 
-        @social_points = @user_points.select{ |event| event.point_type == "social"}; 
-        @service_points = @user_points.select{ |event| event.point_type == "service"}; 
-        @ld_points = @user_points.select{ |event| event.point_type == "ld"}; 
-        @pr_points = @user_points.select{ |event| event.point_type == "pr"}; 
-        
-        
-        
+        @fr_points = @user_points.select{ |event| event.point_type == "fr"}
+        @social_points = @user_points.select{ |event| event.point_type == "social"}
+        @service_points = @user_points.select{ |event| event.point_type == "service"}
+        @ld_points = @user_points.select{ |event| event.point_type == "ld"}
+        @pr_points = @user_points.select{ |event| event.point_type == "pr"} 
     end
     
     def myregistrations
+        netid = session[:cas_user]
+        @user = User.get_user(netid)
+        @user_events = []
+        
+        events_registered = EventAttendance.find_unapproved_events(netid, "unapproved")
+        events_registered.each{ |event_registered|
+            @user_events.push(Event.find(event_registered.event_id))
+        }
+        @fr_events = @user_events.select{ |event| event.point_type == "fr"}
+        @social_events = @user_events.select{ |event| event.point_type == "social"}
+        @service_events = @user_events.select{ |event| event.point_type == "service"}
+        @ld_events = @user_events.select{ |event| event.point_type == "ld"}
+        @pr_events = @user_events.select{ |event| event.point_type == "pr"} 
     end
     
 end
