d509513a997070f884f63c0238c46ed2eead72f2
Colt94 <coltonmonts@gmail.com>
2019-11-12 14:59:12 +0000

    migration

--- ProjectFlex/db/migrate/20191015140826_create_event_attendances.rb
+++ ProjectFlex/db/migrate/20191015140826_create_event_attendances.rb
@@ -3,6 +3,7 @@ class CreateEventAttendances < ActiveRecord::Migration[5.2]
     create_table :event_attendances do |t|
       t.string :user_id
       t.string :user_name
+      t.string :user_zone
       t.string :zone
       t.integer :event_id
       t.string :status, default: "unapproved"
