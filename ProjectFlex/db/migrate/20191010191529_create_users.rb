a038319dabeb66441924ae2daff005adc926980d
Colt94 <coltonmonts@gmail.com>
2019-11-12 14:56:16 +0000

    alted migrations

--- ProjectFlex/db/migrate/20191010191529_create_users.rb
+++ ProjectFlex/db/migrate/20191010191529_create_users.rb
@@ -2,7 +2,9 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :name
-
+      t.string :zone
+      t.string :permissions, default: "Member"
+      t.string :net_id
       t.timestamps
     end
   end
--- ProjectFlex/db/migrate/20191015140826_create_event_attendances.rb
+++ ProjectFlex/db/migrate/20191015140826_create_event_attendances.rb
@@ -2,6 +2,8 @@ class CreateEventAttendances < ActiveRecord::Migration[5.2]
   def change
     create_table :event_attendances do |t|
       t.string :user_id
+      t.string :user_name
+      t.string :zone
       t.integer :event_id
       t.string :status, default: "unapproved"
 
