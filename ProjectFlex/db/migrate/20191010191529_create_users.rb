ae45883e8b8f359cfed4f51c43ed9eddf7c16754
Colt94 <coltonmonts@gmail.com>
2019-11-12 15:07:03 +0000

    migration

--- ProjectFlex/db/migrate/20191010191529_create_users.rb
+++ ProjectFlex/db/migrate/20191010191529_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :zone
+      t.string :user_zone
       t.string :permissions, default: "Member"
       t.string :net_id
       t.timestamps
