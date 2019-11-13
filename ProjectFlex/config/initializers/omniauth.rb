cf815b18e75003434194a7e9e281ae9b66e8155e
Colt94 <coltonmonts@gmail.com>
2019-11-13 23:47:36 +0000

    seeds

--- ProjectFlex/config/initializers/omniauth.rb
+++ ProjectFlex/config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['google_client_id'], ENV['google_client_secret']
+  #provider :google_oauth2, Rails.application.credentials.google[:google_client_id], Rails.application.credentials.aws[:google_client_secret]
 end
