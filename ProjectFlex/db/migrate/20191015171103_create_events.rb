f63e7844cf881d3c0a2f35aeef30e5d097813023
Colt94 <coltonmonts@gmail.com>
2019-11-16 20:48:48 +0000

    added migrations

--- ProjectFlex/db/migrate/20191015171103_create_events.rb
+++ ProjectFlex/db/migrate/20191015171103_create_events.rb
@@ -5,7 +5,11 @@ class CreateEvents < ActiveRecord::Migration[5.2]
       t.string :date
       t.integer :point_value, default: 1
       t.string :point_type
-
+      t.string :location
+      t.string :details
+      t.integer :max_signups
+      t.integer :current_signups
+      t.boolean :mandatory
       t.timestamps
     end
   end
