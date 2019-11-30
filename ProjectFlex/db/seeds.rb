25138b57d1ac73dda3655dc43ace7212cb83a93f
Ubuntu <ubuntu@ip-172-31-29-22.us-east-2.compute.internal>
2019-11-30 21:37:00 +0000

    Removed calendar and forms from sidebar that isn't logged in

--- ProjectFlex/db/seeds.rb
+++ ProjectFlex/db/seeds.rb
@@ -29,7 +29,7 @@ user_list = [
     { :name => "Colin", :permissions => "Member", :net_id => "c.123", :user_zone => "LD"  },
     { :name => "Deyton", :permissions => "Member", :net_id => "d.123", :user_zone => "LD"  },
     { :name => "Jack", :permissions => "Member", :net_id => "e.123", :user_zone => "LD"  },
-    { :name => "Jacob", :permissions => "Member", :net_id => "f.123", :user_zone => "LD"  },
+    { :name => "JacobB", :permissions => "Member", :net_id => "f.123", :user_zone => "LD"  },
     { :name => "Livia", :permissions => "Member", :net_id => "g.123", :user_zone => "LD"  },
     { :name => "Ty", :permissions => "Member", :net_id => "h.123", :user_zone => "LD"  },
     { :name => "Sydney", :permissions => "Member", :net_id => "i.123", :user_zone => "LD"  },
@@ -46,12 +46,15 @@ attendance_list = [
     { :user_id => "gia.123", :user_name => "Gia", :event_id => 1, :status => "submitted", :user_zone => "Service" },
     { :user_id => "evan.123", :user_name => "Evan", :event_id => 3, :status => "approved", :user_zone => "Social" },
     { :user_id => "maggie.123", :user_name => "Maggie", :event_id => 2, :status => "submitted", :user_zone => "PR" },
+    { :user_id => "maggie.123", :user_name => "Maggie", :event_id => 4, :status => "submitted", :user_zone => "PR" },
+    { :user_id => "alex.123", :user_name => "Alex", :event_id => 4, :status => "submitted", :user_zone => "Social" },
 ]
 
 event_list = [
     { :name => "Chick-fil-A Profit Share", :date => "2019-11-15T18:30:00", :point_value => 1, :point_type => "FR", :location => "Chick-fil-A on Texas", :details => "Send a pic of your receipt\nMention Flex to Cashier", :max_signups => 4, :current_signups => 3},
     { :name => "Winter Formal", :date => "2019-11-02T20:00:00", :point_value => 1, :point_type => "Social", :location => "(Winter Formal Address)", :details => "Dress Code: (...)", :current_signups =>  2},
-    { :name => "Big Event", :date => "2019-11-20T07:00:00", :point_value => 1, :point_type => "Service", :location => "(Big Event Address)", :details => "Show up", :current_signups => 2, :mandatory => true }
+    { :name => "Big Event", :date => "2019-11-20T07:00:00", :point_value => 1, :point_type => "Service", :location => "(Big Event Address)", :details => "Show up", :current_signups => 2, :mandatory => true },
+    { :name => "Gen Meeting", :date => "2019-11-25T20:30:00", :point_value => 0, :point_type => "LD", :location => "MSC 2406B", :details => "Show up", :current_signups => 2}
 ]
 
 user_list.each do |user|
